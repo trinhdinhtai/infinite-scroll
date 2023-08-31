2a86b3166207f10da888872b8c2562a347b6588e
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-08-31 22:39:37 +0700

    feat: implementing infinite scroll using intersection observer

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,61 @@
 import Stars from "./components/Stars";
 
 import "./App.css";
-
-const planetsInSolarSystem = [
-	{
-		name: "Mercury",
-		mass: 0.000174,
-		radius: 0.0341,
-		period: 88,
-		semi_major_axis: 0.387098,
-		temperature: 400,
-		distance_light_year: 0.000011,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-	{
-		name: "Venus",
-		mass: 0.00257,
-		radius: 0.0847,
-		period: 224.7,
-		semi_major_axis: 0.723332,
-		temperature: 737,
-		distance_light_year: 0.000004,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-	{
-		name: "Earth",
-		mass: 0.00315,
-		radius: 0.0892,
-		period: 365.2,
-		semi_major_axis: 1,
-		temperature: 288,
-		distance_light_year: 0,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-	{
-		name: "Mars",
-		mass: 0.000338,
-		radius: 0.0488,
-		period: 687,
-		semi_major_axis: 1.542,
-		temperature: 210,
-		distance_light_year: 0.000037,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-	{
-		name: "Jupiter",
-		mass: 1,
-		radius: 1,
-		period: 4331,
-		semi_major_axis: 5.204,
-		temperature: 165,
-		distance_light_year: 0.000088,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-	{
-		name: "Saturn",
-		mass: 0.299,
-		radius: 0.843,
-		period: 10747,
-		semi_major_axis: 9.537,
-		temperature: 134,
-		distance_light_year: 0.00017,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-	{
-		name: "Uranus",
-		mass: 0.0457,
-		radius: 0.358,
-		period: 30589,
-		semi_major_axis: 19.191,
-		temperature: 76,
-		distance_light_year: 0.000304,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-	{
-		name: "Neptune",
-		mass: 0.0537,
-		radius: 0.346,
-		period: 59800,
-		semi_major_axis: 30.07,
-		temperature: 72,
-		Sedistance_light_year: 0.000478,
-		host_star_mass: 1,
-		host_star_temperature: 6000,
-	},
-];
-
-const limit = 30;
+import { useState, useRef, useEffect, useCallback } from "react";
+import { LIMIT, planetsInSolarSystem } from "./constant";
+import Planet from "./components/Planet";
 
 function App() {
-	return (
-		<div className="universe">
-			<div className="stars">
-				<Stars />
-			</div>
-			<h1 className="title">Explore the Universe</h1>
-			<p className="subheading">Journey through the cosmos</p>
-		</div>
-	);
+  const [planets, setPlanets] = useState(planetsInSolarSystem);
+  const [offset, setOffset] = useState(0);
+  const loaderRef = useRef(null);
+
+  const loadMorePlanets = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://planets-api-rho.vercel.app/api/planets?offset=${offset}`
+      );
+      const data = await response.json();
+      setPlanets([...planets, ...data]);
+      setOffset((previousOffset) => previousOffset + LIMIT);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [offset, planets]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const firstEntry = entries[0];
+      if (firstEntry.isIntersecting) {
+        // Load more planets when the loader is visible
+        loadMorePlanets();
+      }
+    });
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    // Clean up the observer on component unmount
+    return () => observer.disconnect();
+  }, [loadMorePlanets]);
+
+  return (
+    <div className="universe">
+      <div className="stars">
+        <Stars />
+      </div>
+      <h1 className="title">Explore the Universe</h1>
+      <p className="subheading">Journey through the cosmos</p>
+
+      <div className="planets">
+        {planets.map((planet) => (
+          <Planet key={planet.name} data={planet} />
+        ))}
+      </div>
+
+      <div ref={loaderRef} className="spinner"></div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -12,112 +12,112 @@ import Neptune from "./PlanetImages/Neptune";
 import Pluto from "./PlanetImages/Pluto";
 import RandomPlanetImage from "./PlanetImages/RandomPlanetImage";
 import {
-	generateRandomColor,
-	getMass,
-	getOrbitalPeriodInYears,
-	getRadiusInKilometers,
-	getTemperature,
+  generateRandomColor,
+  getMass,
+  getOrbitalPeriodInYears,
+  getRadiusInKilometers,
+  getTemperature,
 } from "../utils";
 
 const Planet = React.memo(function Planet({ data }) {
-	const [randomColor] = useState(generateRandomColor());
+  const [randomColor] = useState(generateRandomColor());
 
-	const planetNameInLowerCase = data.name.toLowerCase();
-	const notAvailable = "N/A";
+  const planetNameInLowerCase = data.name.toLowerCase();
+  const notAvailable = "N/A";
 
-	const getImage = (name) => {
-		switch (name) {
-			case "mercury":
-				return <Mercury />;
-			case "venus":
-				return <Venus />;
-			case "earth":
-				return <Earth />;
-			case "mars":
-				return <Mars />;
-			case "jupiter":
-				return <Jupiter />;
-			case "saturn":
-				return <Saturn />;
-			case "uranus":
-				return <Uranus />;
-			case "neptune":
-				return <Neptune />;
-			case "pluto":
-				return <Pluto />;
-			default:
-				return <RandomPlanetImage />;
-		}
-	};
+  const getImage = (name) => {
+    switch (name) {
+      case "mercury":
+        return <Mercury />;
+      case "venus":
+        return <Venus />;
+      case "earth":
+        return <Earth />;
+      case "mars":
+        return <Mars />;
+      case "jupiter":
+        return <Jupiter />;
+      case "saturn":
+        return <Saturn />;
+      case "uranus":
+        return <Uranus />;
+      case "neptune":
+        return <Neptune />;
+      case "pluto":
+        return <Pluto />;
+      default:
+        return <RandomPlanetImage />;
+    }
+  };
 
-	const getBgColor = (name) => {
-		switch (name) {
-			case "mercury":
-				return "#c99090";
-			case "venus":
-				return "#FFB266";
-			case "earth":
-				return "#A1D2FF";
-			case "mars":
-				return "#FF6666";
-			case "jupiter":
-				return "#FFCC99";
-			case "saturn":
-				return "#FFCC66";
-			case "uranus":
-				return "#66b0ff";
-			case "neptune":
-				return "#66CCFF";
-			case "pluto":
-				return "#9c95b0";
-			default:
-				return randomColor;
-		}
-	};
+  const getBgColor = (name) => {
+    switch (name) {
+      case "mercury":
+        return "#c99090";
+      case "venus":
+        return "#FFB266";
+      case "earth":
+        return "#A1D2FF";
+      case "mars":
+        return "#FF6666";
+      case "jupiter":
+        return "#FFCC99";
+      case "saturn":
+        return "#FFCC66";
+      case "uranus":
+        return "#66b0ff";
+      case "neptune":
+        return "#66CCFF";
+      case "pluto":
+        return "#9c95b0";
+      default:
+        return randomColor;
+    }
+  };
 
-	return (
-		<div className="planet planet-entering">
-			<div
-				style={{
-					backgroundColor: getBgColor(planetNameInLowerCase),
-				}}
-				className="planet-image"
-			>
-				{getImage(planetNameInLowerCase)}
-			</div>
-			<div className="planet-content">
-				<h2 className="planet-name">{data.name}</h2>
-				<p className="planet-fact">
-					<span className="planet-fact-title">Mass:</span>{" "}
-					{data.mass ? `${getMass(data.mass)} kg` : notAvailable}
-				</p>
-				<p className="planet-fact">
-					<span className="planet-fact-title">Radius:</span>{" "}
-					{data.radius
-						? `${getRadiusInKilometers(data.radius)} km`
-						: notAvailable}
-				</p>
-				<p className="planet-fact">
-					<span className="planet-fact-title">Orbital Period:</span>{" "}
-					{data.orbitalPeriod
-						? `${getOrbitalPeriodInYears(data.orbitalPeriod)} year${
-								data.period === 365.2 ? "" : "s"
-						  }`
-						: notAvailable}
-				</p>
-				<p className="planet-fact">
-					<span className="planet-fact-title">Temperature:</span>{" "}
-					{data.temperature
-						? `${getTemperature(data.temperature)}°C`
-						: notAvailable}
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="planet planet-entering">
+      <div
+        style={{
+          backgroundColor: getBgColor(planetNameInLowerCase),
+        }}
+        className="planet-image"
+      >
+        {getImage(planetNameInLowerCase)}
+      </div>
+      <div className="planet-content">
+        <h2 className="planet-name">{data.name}</h2>
+        <p className="planet-fact">
+          <span className="planet-fact-title">Mass:</span>{" "}
+          {data.mass ? `${getMass(data.mass)} kg` : notAvailable}
+        </p>
+        <p className="planet-fact">
+          <span className="planet-fact-title">Radius:</span>{" "}
+          {data.radius
+            ? `${getRadiusInKilometers(data.radius)} km`
+            : notAvailable}
+        </p>
+        <p className="planet-fact">
+          <span className="planet-fact-title">Orbital Period:</span>{" "}
+          {data.orbitalPeriod
+            ? `${getOrbitalPeriodInYears(data.orbitalPeriod)} year${
+                data.period === 365.2 ? "" : "s"
+              }`
+            : notAvailable}
+        </p>
+        <p className="planet-fact">
+          <span className="planet-fact-title">Temperature:</span>{" "}
+          {data.temperature
+            ? `${getTemperature(data.temperature)}°C`
+            : notAvailable}
+        </p>
+      </div>
+    </div>
+  );
 });
 
 Planet.propTypes = {
-	data: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
 };
 
 export default Planet;
